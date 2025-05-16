1a8af75572da5b43d4f203f65ff1ab6d61250603
eduardofborges55 <borgeseduardof55@gmail.com>
2025-05-16 09:03:02 -0300

    atualizando

--- routes.js
+++ routes.js
@@ -1,5 +1,7 @@
 import { Router } from "express"
 import { database } from "./db.js"
+import req from "express/lib/request.js";
+import res, { status } from "express/lib/response.js";
 const dadosUsuario = [
     "name","email","password"
 ] 
@@ -9,7 +11,14 @@ userRoutes.get("",(req,res)=>{
 
     const users = database.users
     return res.status(200).json(users)
-})
+});
+
+userRoutes.get("/:id",(req,res)=>{
+    const findUser = database.users.find((user)=>user.id === req.params.id)
+    if(!findUser){
+        return res.status(200).json(findUser)
+    }
+});
 userRoutes.post("",(req,res)=>{
     const infos = Object.keys(req.body)
     console.log(infos,"infos")
@@ -33,16 +42,28 @@ userRoutes.post("",(req,res)=>{
  
     database.users.push(user)
     return res.status(201).json(user)
-})
-userRoutes.get("/:id",(req,res)=>{
-    const findUser = database.users.find((user)=>user.id === req.params.id)
-    if(!findUser){
-        return res.status(200).json(findUser)
-    }
-})
+});
 
-// if( dadosUsuario != "name" && dadosUsuario != "email" && "password" ){
-//     alert("tem que ter: nome, email e senha nada mais")
-// } else {
-//     alert(dadosUsuario)
-// }
+userRoutes.put("/:id", (req, res) => {
+    const index = database.users.findIndex(user => user.id === req.params.id);
+    if(index === -1) return res.status(404).json({ error: "Usuário não existe" });
+    
+    const usuarioAtualizado = {};
+    for(const dados of dadosUsuario) {
+        if(!req.body[dados]) {
+            return res.status(400).json({ error: `Faltando dados: ${dados}`});
+        }
+        usuarioAtualizado[dados] = req.body[dados];
+    }
+        usuarioAtualizado.id = req.params.id;
+        database.users[index] = usuarioAtualizado;
+        return res.json(usuarioAtualizado);
+    });
+
+    userRoutes.delete("/:d", (req, res) => {
+        const index = database.users.findIndex(user => user.id === req.params.id);
+        if(index === -1) return res.status(404).json({error: "Usuário não existe"});
+
+        database.users.splice(index, 1);
+        return res.json({ message: "Usuário removido"});
+    });
